b8263acb3e7b8cd55841446122abe5cf583fbde1
Muneeb-Shafi <93155715+Muneeb-Shafi@users.noreply.github.com>
2022-03-05 15:06:49 +0500

    Initial Commit

---
+++ ChatClient.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Assignment_2
+{
+    partial class ChatClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.grpMessageWindow = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.grpUserCredentials = new System.Windows.Forms.GroupBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblLoginName = new System.Windows.Forms.Label();
+            this.grpMessageWindow.SuspendLayout();
+            this.grpUserCredentials.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(258, 11);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "&Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // grpMessageWindow
+            // 
+            this.grpMessageWindow.Controls.Add(this.btnSend);
+            this.grpMessageWindow.Controls.Add(this.txtSendMessage);
+            this.grpMessageWindow.Controls.Add(this.rtbMessages);
+            this.grpMessageWindow.Enabled = false;
+            this.grpMessageWindow.Location = new System.Drawing.Point(12, 58);
+            this.grpMessageWindow.Name = "grpMessageWindow";
+            this.grpMessageWindow.Size = new System.Drawing.Size(516, 296);
+            this.grpMessageWindow.TabIndex = 3;
+            this.grpMessageWindow.TabStop = false;
+            this.grpMessageWindow.Text = "Message window";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(435, 235);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 55);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(9, 235);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(408, 55);
+            this.txtSendMessage.TabIndex = 1;
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(9, 33);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.ReadOnly = true;
+            this.rtbMessages.Size = new System.Drawing.Size(501, 190);
+            this.rtbMessages.TabIndex = 0;
+            this.rtbMessages.Text = "";
+            // 
+            // grpUserCredentials
+            // 
+            this.grpUserCredentials.Controls.Add(this.btnLogin);
+            this.grpUserCredentials.Controls.Add(this.txtUserName);
+            this.grpUserCredentials.Controls.Add(this.lblLoginName);
+            this.grpUserCredentials.Location = new System.Drawing.Point(12, 12);
+            this.grpUserCredentials.Name = "grpUserCredentials";
+            this.grpUserCredentials.Size = new System.Drawing.Size(339, 40);
+            this.grpUserCredentials.TabIndex = 4;
+            this.grpUserCredentials.TabStop = false;
+            this.grpUserCredentials.Text = "User credentials:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(77, 12);
+            this.txtUserName.MaxLength = 10;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(158, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // lblLoginName
+            // 
+            this.lblLoginName.AutoSize = true;
+            this.lblLoginName.Location = new System.Drawing.Point(6, 16);
+            this.lblLoginName.Name = "lblLoginName";
+            this.lblLoginName.Size = new System.Drawing.Size(65, 13);
+            this.lblLoginName.TabIndex = 0;
+            this.lblLoginName.Text = "Login name:";
+            // 
+            // ChatClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 366);
+            this.Controls.Add(this.grpMessageWindow);
+            this.Controls.Add(this.grpUserCredentials);
+            this.Name = "ChatClient";
+            this.Text = "ChatClient";
+            this.grpMessageWindow.ResumeLayout(false);
+            this.grpMessageWindow.PerformLayout();
+            this.grpUserCredentials.ResumeLayout(false);
+            this.grpUserCredentials.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.GroupBox grpMessageWindow;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSendMessage;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.GroupBox grpUserCredentials;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblLoginName;
+    }
+}
---
+++ ChatClient.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace Assignment_2
+{
+    [ServiceContract(CallbackContract = typeof(IChatService))]
+    public interface IChatService
+    {
+        [OperationContract(IsOneWay = true)]
+        void Join(string memberName);
+        [OperationContract(IsOneWay = true)]
+        void Leave(string memberName);
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string memberName, string message);
+    }
+
+    public interface IChatChannel : IChatService, IClientChannel
+    {
+    }
+
+    public partial class ChatClient : Form, IChatService
+    {
+        private delegate void UserJoined(string name);
+        private delegate void UserSendMessage(string name, string message);
+        private delegate void UserLeft(string name);
+
+        private static event UserJoined NewJoin;
+        private static event UserSendMessage MessageSent;
+        private static event UserLeft RemoveUser;
+
+        private string userName;
+        private IChatChannel channel;
+        private DuplexChannelFactory<IChatChannel> factory;
+
+        private static string[] knockName = { "Says",
+                                     "Water",
+                                     "Nobel",
+                                     "Annie",
+                                     "Wa",
+                                     "I am",
+                                     "Haven",
+                                     "Spell",
+                                     "To",
+                                     "Tank"
+        };
+
+        //Creating the proivate static method array string of knock knock jokes that will be sent to the client on the basis of the input from the user.
+        private static string[] knockJoke = {
+                                    "Says me, that's who!",
+                                    "Water you asking so many questions for, just open up!",
+                                    "Nobel ... that's why I knocked!",
+                                    "Annie thing you can do I can better!",
+                                    "What are you so excited about?!><",
+                                    "Dont you even know who you are? :/",
+                                    "Haven you heard enough of these knock-knock jokes?",
+                                    "W-H-O!",
+                                    "No, it's to whom!",
+                                    "You are welcome"
+        };
+
+        private static Random randomArray = new Random();
+
+        public ChatClient()
+        {
+            InitializeComponent();
+            this.AcceptButton = btnLogin;
+        }
+
+        public ChatClient(string userName)
+        {
+            this.userName = userName;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUserName.Text.Trim()))
+            {
+                try
+                {
+                    NewJoin += new UserJoined(ChatClient_NewJoin);
+                    MessageSent += new UserSendMessage(ChatClient_MessageSent);
+                    RemoveUser += new UserLeft(ChatClient_RemoveUser);
+
+                    channel = null;
+                    this.userName = txtUserName.Text.Trim();
+                    InstanceContext context = new InstanceContext(
+                        new ChatClient(txtUserName.Text.Trim()));
+                    factory =
+                        new DuplexChannelFactory<IChatChannel>(context, "ChatEndPoint");
+                    channel = factory.CreateChannel();
+                    IOnlineStatus status = channel.GetProperty<IOnlineStatus>();
+                    status.Offline += new EventHandler(Offline);
+                    status.Online += new EventHandler(Online);
+                    channel.Open();
+                    channel.Join(this.userName);
+                    grpMessageWindow.Enabled = true;
+                    grpUserCredentials.Enabled = false;
+                    this.AcceptButton = btnSend;
+                    rtbMessages.AppendText("*****************************WEL-COME to Chat Application*****************************\r\n");
+                    txtSendMessage.Select();
+                    txtSendMessage.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        void ChatClient_RemoveUser(string name)
+        {
+            try
+            {
+                rtbMessages.AppendText("\r\n");
+                rtbMessages.AppendText(name + " left at " + DateTime.Now.ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.ToString());
+            }
+        }
+
+        void ChatClient_MessageSent(string name, string message)
+        {
+            rtbMessages.AppendText("\r\n");
+            rtbMessages.AppendText(name + " says: " + message);
+            string messageRec_1;
+
+            bool ValidOuterInput = false;
+            bool ValidInnerLoop = false;
+
+            byte[] knockknockMessage = Encoding.ASCII.GetBytes("Knock Knock!");
+
+            while (!ValidOuterInput)
+            {
+                messageRec_1 = string.Empty;
+
+
+                messageRec_1 += message;
+
+                if (messageRec_1 == "Who's there?")
+                {
+                    ValidOuterInput = true;
+
+                    int rand = randomArray.Next(knockName.Length);
+
+                    Console.WriteLine("Text received : {0}", messageRec_1);
+
+                    byte[] bytesSent_1 = Encoding.ASCII.GetBytes(knockName[rand]);
+
+
+                    while (!ValidInnerLoop)
+                    {
+
+
+                        int bytesRec_2 = message.Length;
+
+
+                        {
+                            ValidInnerLoop = true;
+
+                            byte[] bytesSent_2 = Encoding.ASCII.GetBytes(knockJoke[rand]);
+
+                        }
+
+                        {
+                            ValidInnerLoop = false;
+                            byte[] messageInvalid_2 = Encoding.ASCII.GetBytes("Error:Please input a valid string -> recieved_Name + who?. \n\tFor Example: 'Sid who?'. Ask Now properly! I am knocking");
+                        }
+                    }
+                }
+                else if (messageRec_1 == "quit")
+                {
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Text received : {0}", messageRec_1);
+                    byte[] messageInvalid_1 = Encoding.ASCII.GetBytes("Please enter a valid response -> 'Who's there?'");
+                }
+
+
+            }//end of outer while loop
+
+        }//end for loop
+
+        public void Join(string memberName)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IChatService.Leave(string memberName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string memberName, string message)
+        {
+            throw new NotImplementedException();
+        }
+    
+        void ChatClient_NewJoin(string name)
+        {
+            rtbMessages.AppendText("\r\n");
+            rtbMessages.AppendText(name + " joined at: [" + DateTime.Now.ToString() + "]");
+        }
+
+        void Online(object sender, EventArgs e)
+        {
+            rtbMessages.AppendText("\r\nOnline: " + this.userName);
+        }
+
+        void Offline(object sender, EventArgs e)
+        {
+            rtbMessages.AppendText("\r\nOffline: " + this.userName);
+        }
+
+        #region IChatService Members
+/*
+        public void Join(string memberName)
+        {
+            if (NewJoin != null)
+            {
+                NewJoin(memberName);
+            }
+        }*/
+
+        public new void Leave(string memberName)
+        {
+            if (RemoveUser != null)
+            {
+                RemoveUser(memberName);
+            }
+        }
+/*
+        public void SendMessage(string memberName, string message)
+        {
+            if (MessageSent != null)
+            {
+                MessageSent(memberName, message);
+            }
+        }
+*/
+        #endregion
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            channel.SendMessage(this.userName, txtSendMessage.Text.Trim());
+            txtSendMessage.Clear();
+            txtSendMessage.Select();
+            txtSendMessage.Focus();
+        }
+
+        private void ChatClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (channel != null)
+                {
+                    channel.Leave(this.userName);
+                    channel.Close();
+                }
+                if (factory != null)
+                {
+                    factory.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void grpMessageWindow_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSendMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbMessages_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpUserCredentials_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLoginName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpUserList_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
